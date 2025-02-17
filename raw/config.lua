f2dc5410f1d88d31db306a9273da15ca18625614
Dzgak <dev.dzgak@gmail.com>
2025-02-17 21:28:07 +0500

    Update config.lua

--- raw/config.lua
+++ raw/config.lua
@@ -2,6 +2,6 @@ return {
     github = {
         repository = "Dzgak/Loader",
         branch = "main",
-        raw_base = `https://raw.githubusercontent.com/{repository}/main/raw`
+        raw_base = string.format("https://raw.githubusercontent.com/%s/%s/raw", "Dzgak/Loader", "main")
     }
 }
