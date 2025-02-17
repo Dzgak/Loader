5f159c75881410e9ffa7566b2b0ecd439e2fd082
Dzgak <dev.dzgak@gmail.com>
2025-02-17 11:40:42 +0500

    Refactor project structure by removing unused game1.lua script, adding registry.lua for script management, and updating .gitignore to exclude registry.lua

---
+++ raw/registry.lua
@@ -0,0 +1,13 @@
+return {
+    scripts = { {
+    placeIds = { 324234, 324234 },
+    gameId = 987322365,
+    path = 'scripts/example copy.lua',
+    name = 'example copy'
+}, {
+    placeIds = { 1234567, 7654321 },
+    gameId = 98765,
+    path = 'scripts/example.lua',
+    name = 'example'
+} }
+}
